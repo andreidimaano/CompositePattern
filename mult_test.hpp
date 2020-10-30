1f560bddff4d3b4a60c872f14ce8f1706f822394
andreidimaano <andreiidimaano@gmail.com>
2020-10-30 15:03:14 -0700

    Fixes #7 change tests for design pattern

--- mult_test.hpp
+++ mult_test.hpp
@@ -82,58 +82,58 @@ TEST(MultTest, MultWholeNumTwoAdd) {
 
 //Double
 TEST(MultTest, MultDouble) {
-	Op* left = new Op(10.5);
-	Op* right = new Op(5.5);
-	Mult* test = new Mult(left, right);
+	Base* left = new Op(10.5);
+	Base* right = new Op(5.5);
+	Base* test = new Mult(left, right);
 	EXPECT_DOUBLE_EQ(test->evaluate(), 57.75);
 }
 
 TEST(MultTest, MultDoubleString) {
-	Op* left = new Op(10.5);
-	Op* right = new Op(5.5);
-	Mult* test = new Mult(left, right);
+	Base* left = new Op(10.5);
+	Base* right = new Op(5.5);
+	Base* test = new Mult(left, right);
 	std::string str = "10.500000 * 5.500000";
 	EXPECT_EQ(test->stringify(), str);
 }
 
 //Double using one Add as an op
 TEST(MultTest, MultDoubleAddString) {
-	Op* addOne = new Op(10.5);
-    Op* addTwo = new Op(10.5);
-	Op* right = new Op(5.5);
-    Add* left = new Add(addOne, addTwo); //10 + 10
-	Mult* test = new Mult(left, right);
+	Base* addOne = new Op(10.5);
+        Base* addTwo = new Op(10.5);
+	Base* right = new Op(5.5);
+        Base* left = new Add(addOne, addTwo); //10 + 10
+	Base* test = new Mult(left, right);
 	std::string str = "10.500000 + 10.500000 * 5.500000"; //no parenthesis but should be (10+10) * 5
 	EXPECT_EQ(test->stringify(), str);
 }
 
 TEST(MultTest, MultDoubleNumAdd) {
-	Op* addOne = new Op(10.5);
-    Op* addTwo = new Op(10.5);
-	Op* right = new Op(5.5);
-    Add* left = new Add(addOne, addTwo); //10 + 10 = 20  * 5
-	Mult* test = new Mult(left, right);
+	Base* addOne = new Op(10.5);
+        Base* addTwo = new Op(10.5);
+	Base* right = new Op(5.5);
+        Base* left = new Add(addOne, addTwo); //10 + 10 = 20  * 5
+	Base* test = new Mult(left, right);
 	EXPECT_DOUBLE_EQ(test->evaluate(), 115.5);
 }
 
 //Double using Two Add as an op
 TEST(MultTest, MultDoubleTwoAddString) {
-	Op* addOne = new Op(10.5);
-    Op* addTwo = new Op(10.5);
-    Op* rightOne = new Op(10.5);
-    Op* rightTwo = new Op(10.5);
-    Add* left = new Add(addOne, addTwo); //10 + 10
-    Add* right = new Add(rightOne, rightTwo);
-	Mult* test = new Mult(left, right);
+	Base* addOne = new Op(10.5);
+        Base* addTwo = new Op(10.5);
+        Base* rightOne = new Op(10.5);
+        Base* rightTwo = new Op(10.5);
+    	Base* left = new Add(addOne, addTwo); //10 + 10
+    	Base* right = new Add(rightOne, rightTwo);
+	Base* test = new Mult(left, right);
  	std::string str = "10.500000 + 10.500000 * 10.500000 + 10.500000"; //no parenthesis but should be (10+10) * 5
 	EXPECT_EQ(test->stringify(), str);
 }
 
 TEST(MultTest,MultDoubleTwoAdd) {
-	Op* addOne = new Op(10.5); Op* addTwo = new Op(10.5);
-    Op* rightOne = new Op(10.5); Op* rightTwo = new Op(10.5);
-    Add* left = new Add(addOne, addTwo); Add* right = new Add(rightOne, rightTwo);
-	Mult* test = new Mult(left, right);
+	Base* addOne = new Op(10.5); Op* addTwo = new Op(10.5);
+    	Base* rightOne = new Op(10.5); Op* rightTwo = new Op(10.5);
+    	Base* left = new Add(addOne, addTwo); Add* right = new Add(rightOne, rightTwo);
+	Base* test = new Mult(left, right);
 	EXPECT_DOUBLE_EQ(test->evaluate(), 441);
 }
 
